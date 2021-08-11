115db906a1ad0221c32aad61fad6f65f39992781
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-11 13:45:37 +0300

    context start MOvie list movie page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-// import Counter from './components/Counter';
-import CounterHook from './components/CounterHook';
+import MoviePage from './context/MoviePage';
+// import CounterHook from './components/CounterHook';
 
 function App() {
   return (
     <div className="App">
-      {/* <Counter /> */}
-      <CounterHook />
+      <MoviePage />
+      
     </div>
   );
 }
---
+++ src/context/MovieList.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class MovieList extends Component {
+  state = {};
+  render() {
+    return <div>MovieList</div>;
+  }
+}
+
+export default MovieList;
