c3df0da3115c7e29f4115ec4b7b874ed4b44bd91
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-11 14:32:08 +0300

    how to consume Context not in render

--- src/context/MovieList.jsx
+++ src/context/MovieList.jsx
@@ -2,19 +2,27 @@ import React, { Component } from "react";
 import UserContext from "./UserContext";
 
 class MovieList extends Component {
-  state = {};
+  componentDidMount() {
+    console.log("MovieList mounted to dom");
+    console.log("context in mount", this.context); // { name: "James", age: 41 }
+  }
   render() {
     return (
       <UserContext.Consumer>
         {(userContext) => (
           <div>
             <h3>MovieList</h3>
-            <p>{userContext.name} user likes this movie list and he is {userContext.age} years old</p>
+            <p>
+              {userContext.name} user likes this movie list and he is{" "}
+              {userContext.age} years old
+            </p>
           </div>
         )}
       </UserContext.Consumer>
     );
   }
 }
+// pridedam contexta kad butu pasiekiamas visoje klase
+MovieList.contextType = UserContext;
 
 export default MovieList;
