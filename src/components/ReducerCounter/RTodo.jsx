23051d9376ce48f2a82d4fa2ae3a11cb670fce11
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-18 10:25:50 +0300

    toggle todo functionality

--- src/components/ReducerCounter/RTodo.jsx
+++ src/components/ReducerCounter/RTodo.jsx
@@ -4,11 +4,11 @@ import { ACTION } from "./ReducerSimpleTodo";
 export default function RTodo({ item, dispatch }) {
   return (
     <li>
-      <span>{item.title}</span>
+      <span style={{ color: item.done ? "lightgray" : "black" }}>
+        {item.title}
+      </span>
       <button
-        onClick={() =>
-          dispatch({ type: ACTION.TOGGLE_TODO, payload: item.title })
-        }
+        onClick={() => dispatch({ type: ACTION.TOGGLE_TODO, payload: item.id })}
       >
         Toggle
       </button>
