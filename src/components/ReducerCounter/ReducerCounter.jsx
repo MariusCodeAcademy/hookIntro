3e9a432a2251825037f02f193bac90254855b1e1
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-18 08:50:16 +0300

    reducer + - reset works with ifs

--- src/components/ReducerCounter/ReducerCounter.jsx
+++ src/components/ReducerCounter/ReducerCounter.jsx
@@ -1,22 +1,42 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+
+function countReducer(state, action) {
+  if (action.type === "increment") {
+    return { count: state.count + 1 };
+  }
+  if (action.type === "decrement") {
+    return { count: state.count - 1 };
+  }
+  if (action.type === "reset") {
+    return { count: 0 };
+  }
+}
 
 function ReducerCounter() {
   // useState padaryti kad veiktu counteris
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(countReducer, { count: 0 });
 
   function increment() {
-    setCount(count + 1);
+    // setCount(count + 1);
+    dispatch({ type: "increment" });
   }
   function decrement() {
-    setCount(count - 1);
+    // setCount(count - 1);
+    dispatch({ type: "decrement" });
+  }
+  function resetCounter() {
+    // setCount(count - 1);
+    dispatch({ type: "reset" });
   }
 
   return (
     <div>
       <h4>ReducerCounter</h4>
-      <h2>{count}</h2>
+      <h2>{state.count}</h2>
       <button onClick={increment}>+</button>
       <button onClick={decrement}>-</button>
+      <button onClick={resetCounter}>reset</button>
     </div>
   );
 }
